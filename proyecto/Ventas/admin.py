4fc9c99495bc840172a2506f3786d8f8ff38440a
GMXAndres <gmxandres11@gmail.com>
2023-02-15 17:53:19 -0500

    1.0

---
+++ proyecto/Ventas/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from Ventas.models import Cliente,Producto
+
+# Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'telefono', 'codigo')
+    search_fields = ['nombre']
+    readonly_fields=('created','updated')
+    filter_horizontal=()
+    list_filter=()
+    fieldsets=()
+
+admin.site.register(Cliente,ClienteAdmin)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('descripcion', 'cantidad', 'costo','codigo')
+    search_fields = ['descripcion']
+    readonly_fields=('created','updated')
+    filter_horizontal=()
+    list_filter=()
+    fieldsets=()
+
+admin.site.register(Producto, ProductoAdmin)
